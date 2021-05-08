0ea86c50b5f56986455ef86a4fe0ae130b986902
i-t-m-a-n <mts04@inbox.ru>
2021-05-08 23:21:56 +0000

    change pin for servo

--- 1 low/servo on python/servo2.py
+++ 1 low/servo on python/servo2.py
@@ -6,8 +6,8 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 # Set pin 11 as an output, and define as servo1 as PWM pin
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
+GPIO.setup(8,GPIO.OUT)
+servo1 = GPIO.PWM(8,50) # pin 11 for servo1, pulse 50Hz
 
 # Start PWM running, with value of 0 (pulse off)
 servo1.start(0)
