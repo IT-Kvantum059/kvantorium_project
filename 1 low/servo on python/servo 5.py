01bad7468536a0bc961ea84039162465c6536d9f
i-t-m-a-n <mts04@inbox.ru>
2021-05-09 04:53:57 +0500

    update

--- 1 low/servo on python/servo 5.py
+++ 1 low/servo on python/servo 5.py
@@ -34,19 +34,27 @@ try:
             servo1.ChangeDutyCycle(2+(angle/18))
             time.sleep(0.5)
             servo1.ChangeDutyCycle(0)
+        break
+
+
     elif num_servo == 2:
         while True:
             angle = float(input('Enter for servo2 angle between 0 & 180: '))
             servo2.ChangeDutyCycle(2+(angle/18))
             time.sleep(0.5)
             servo2.ChangeDutyCycle(0)
+        break
+
+
     elif num_servo == 3:
         while True:
             angle = float(input('Enter for servo3 angle between 0 & 180: '))
             servo3.ChangeDutyCycle(2+(angle/18))
             time.sleep(0.5)
             servo3.ChangeDutyCycle(0)
-    break
+        break
+
+    
 
 finally:
     #Clean things up at the end
