835dd76df4a597b89ad90b5bd1ae42757a37988e
i-t-m-a-n <mts04@inbox.ru>
2021-05-19 17:02:31 +0000

    upd

---
+++ 1 low/servo on python/for3servo copy.py
@@ -0,0 +1,75 @@
+# Import libraries
+import RPi.GPIO as GPIO
+import time
+
+
+INIT = 0
+IDLE = 1
+EXEC = 2
+
+state = INIT
+
+while True:
+    if state is INIT:
+        # Set GPIO numbering mode
+        GPIO.setmode(GPIO.BOARD)
+
+        # Set pin as an output, and define as servo as PWM pin
+        # Servo 1
+        GPIO.setup(8,GPIO.OUT)
+        servo1 = GPIO.PWM(8,50)
+
+        # Servo 2
+        GPIO.setup(10,GPIO.OUT)
+        servo2 = GPIO.PWM(10,50)
+
+        # Servo 3
+        GPIO.setup(12,GPIO.OUT)
+        servo3 = GPIO.PWM(12,50)
+
+        # Start PWM running, with value of 0 (pulse off)
+        servo1.start(0)
+        servo2.start(0)
+        servo3.start(0)
+
+        num_servo = int(input('Введите номер сервопривода от 1 до 3: '))
+
+        state = IDLE
+
+    if state is IDLE:
+        if num_servo == 1:
+            angle = float(input('Введите число от 0 до 180 для servo1: '))
+            servo1.ChangeDutyCycle(2+(angle/18))
+            time.sleep(0.5)
+            servo1.ChangeDutyCycle(0)
+
+        elif num_servo == 2:
+            angle = float(input('Введите число от 0 до 180 для servo2: '))
+            servo2.ChangeDutyCycle(2+(angle/18))
+            time.sleep(0.5)
+            servo2.ChangeDutyCycle(0)
+
+        elif num_servo == 3:
+            angle = float(input('Введите число от 0 до 180 для servo3: '))
+            servo3.ChangeDutyCycle(2+(angle/18))
+            time.sleep(0.5)
+            servo3.ChangeDutyCycle(0)
+
+        else:
+            print("Я не понимаю вас")
+
+        state = EXEC
+
+    if state is EXEC:
+        # initialize
+        state = INIT
+
+
+
+# finally:
+#     #Clean things up at the end
+#     servo1.stop()
+#     servo2.stop()
+#     servo3.stop()
+#     GPIO.cleanup()
+#     print("Servo Stop")
